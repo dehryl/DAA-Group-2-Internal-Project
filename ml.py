b68b8a0050f2c7d970fdf2da45431a991986ee9c
Moe <moe.moepyesone77@gmail.com>
2025-11-26 11:25:00 +0800

    edit

--- ml.py
+++ ml.py
@@ -16,9 +16,9 @@ from sklearn.pipeline import Pipeline
 pd.set_option('display.max_columns',100)
 
 df = pd.read_csv('HDB_Resale_Prices.csv')
-#print(df.head())
-#print(df.info())
-#print(df.isnull().sum())
+print(df.head())
+print(df.info())
+print(df.isnull().sum())
 
 df['month'] = pd.to_datetime(df['month'])
 df['sale_year'] = df['month'].dt.year
@@ -64,48 +64,48 @@ df['lease_age_2025'] = 2025 - df['lease_commence_date']
 
 # print(df.describe())
 df['resale_price'] = df['resale_price']
-#print(df['resale_price'].head())
+print(df['resale_price'].head())
 
 df = df[(df['floor_area_sqm'] > 20) & (df['floor_area_sqm'] < 300)]
 
-# plt.figure(figsize=(8,5))
-# sns.histplot(df['resale_price'],bins=50,kde=True)
-# plt.title('Distribution of Resale Prices')
-# plt.xlabel('Resale Price')
-# plt.ylabel('Count')
-# plt.show()
+plt.figure(figsize=(8,5))
+sns.histplot(df['resale_price'],bins=50,kde=True)
+plt.title('Distribution of Resale Prices')
+plt.xlabel('Resale Price')
+plt.ylabel('Count')
+plt.show()
 
-# plt.figure(figsize=(8,5))
-# sns.scatterplot(data=df,x='floor_area_sqm',y='resale_price',alpha=0.4)
-# plt.title('Floor Area vs Resale Price')
-# plt.show()
+plt.figure(figsize=(8,5))
+sns.scatterplot(data=df,x='floor_area_sqm',y='resale_price',alpha=0.4)
+plt.title('Floor Area vs Resale Price')
+plt.show()
 
-# plt.figure(figsize=(8,5))
-# sns.scatterplot(data=df,x='remaining_lease_years',y='resale_price',alpha=0.4)
-# plt.title('Remaining Lease Years vs Resale Price')
-# plt.show()
+plt.figure(figsize=(8,5))
+sns.scatterplot(data=df,x='remaining_lease_years',y='resale_price',alpha=0.4)
+plt.title('Remaining Lease Years vs Resale Price')
+plt.show()
 
-# plt.figure(figsize=(10,6))
-# sns.boxplot(data=df, x='flat_type', y='resale_price')
-# plt.title('Resale Price by Flat Type')
-# plt.xticks(rotation=45)
-# plt.show()
+plt.figure(figsize=(10,6))
+sns.boxplot(data=df, x='flat_type', y='resale_price')
+plt.title('Resale Price by Flat Type')
+plt.xticks(rotation=45)
+plt.show()
 
-# town_mean = df.groupby('town')['resale_price'].mean().sort_values(ascending=False).head(15)
-# plt.figure(figsize=(10,6))
-# sns.barplot(x=town_mean.values,y=town_mean.index)
-# plt.title('Average Resale Price by Town (Top 15)')
-# plt.xlabel('Average Resale Price')
-# plt.ylabel('Town')
-# plt.show()
+town_mean = df.groupby('town')['resale_price'].mean().sort_values(ascending=False).head(15)
+plt.figure(figsize=(10,6))
+sns.barplot(x=town_mean.values,y=town_mean.index)
+plt.title('Average Resale Price by Town (Top 15)')
+plt.xlabel('Average Resale Price')
+plt.ylabel('Town')
+plt.show()
 
-# year_mean = df.groupby('sale_year')['resale_price'].mean()
-# plt.figure(figsize=(8,5))
-# sns.lineplot(x=year_mean.index,y=year_mean.values,marker='o')
-# plt.title('Average Resale Price Over Years')
-# plt.xlabel('Year')
-# plt.ylabel('Average Resale Price')
-# plt.show()
+year_mean = df.groupby('sale_year')['resale_price'].mean()
+plt.figure(figsize=(8,5))
+sns.lineplot(x=year_mean.index,y=year_mean.values,marker='o')
+plt.title('Average Resale Price Over Years')
+plt.xlabel('Year')
+plt.ylabel('Average Resale Price')
+plt.show()
 
 mature_towns = {
     'ANG MO KIO', 'BEDOK', 'BISHAN', 'BUKIT MERAH', 'CLEMENTI',
@@ -169,10 +169,10 @@ y_pred_linreg = linreg_model.predict(x_test)
 r2_linreg = r2_score(y_test,y_pred_linreg)
 rmse_linreg = rmse(y_test,y_pred_linreg)
 
-# print('Average Price: {:,.0f}$'.format(np.average(df['resale_price'])))
+print('Average Price: {:,.0f}$'.format(np.average(df['resale_price'])))
 
-# print(f"Linear Regression R²:   {r2_linreg:.3f}")
-# print(f"Linear Regression RMSE: {rmse_linreg:,.0f}")
+print(f"Linear Regression R²:   {r2_linreg:.3f}")
+print(f"Linear Regression RMSE: {rmse_linreg:,.0f}")
 
 rf_model = Pipeline([
     ('preprocessor',preprocessor),
@@ -192,10 +192,9 @@ y_pred_rf = rf_model.predict(x_test)
 r2_rf = r2_score(y_test,y_pred_rf)
 rmse_rf = rmse(y_test,y_pred_rf)
 
-# print(f"Random Forest R²:   {r2_rf:.3f}")
-# print(f"Random Forest RMSE: {rmse_rf:,.0f}")
-# print(f"Random Forest RMSE: {rmse_rf:,.0f}")
-# print(f"Random Forest Accuracy: {accuracy_rf:,.0f}")
+print(f"Random Forest R²:   {r2_rf:.3f}")
+print(f"Random Forest RMSE: {rmse_rf:,.0f}")
+print(f"Random Forest RMSE: {rmse_rf:,.0f}")
 
 results = pd.DataFrame({
     'Model': ['Linear Regression','Random Forest'],
@@ -216,24 +215,24 @@ feature_df = pd.DataFrame({
     'importance':importances
 }).sort_values(by='importance',ascending=False)
 
-# plt.figure(figsize=(8,10))
-# sns.barplot(data=feature_df.head(20),x='importance',y='feature')
-# plt.title('Top 20 Feature Importances from Random Forest')
-# plt.xlabel('Importance')
-# plt.ylabel('Feature')
-# plt.tight_layout()
-# plt.show()
+plt.figure(figsize=(8,10))
+sns.barplot(data=feature_df.head(20),x='importance',y='feature')
+plt.title('Top 20 Feature Importances from Random Forest')
+plt.xlabel('Importance')
+plt.ylabel('Feature')
+plt.tight_layout()
+plt.show()
 
 type_price = (df[df['flat_type'].isin(['3 ROOM','5 ROOM'])]
               .groupby('flat_type')['resale_price']
               .agg(['mean','median','count'])
               .sort_index())
-# print(type_price)
+print(type_price)
 
 corr_by_town = (df.groupby('town')[['remaining_lease_years','resale_price']]
                 .corr()
                 .unstack()
                 ['remaining_lease_years']['resale_price']
                 .sort_values(ascending=True))
-# print(corr_by_town)
+print(corr_by_town)
 
